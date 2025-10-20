14eafe9bc39a0a0ddbbdc2b23ccb6dd7ed5a4017
torres-xyz <43039035+torres-xyz@users.noreply.github.com>
2025-10-20 23:37:28 +0100

    audio_test still fails but now audio file loads

--- tests/audio_test.cpp
+++ tests/audio_test.cpp
@@ -1,11 +1,15 @@
 #include "../src/audio.hpp"
+#include "raylib-cpp.hpp"
 
 #include <Sound.hpp>
 #include <catch2/catch_test_macros.hpp>
 
 TEST_CASE("Sound should play", "[audio]")
 {
-    raylib::Sound testSound = raylib::Sound("resources/audio/sounds/card_place_01.ogg");
+    const raylib::Window window(0, 0, "Tests");
+    InitAudioDevice();
+
+    const raylib::Sound testSound = raylib::Sound("resources/audio/sounds/card_place_01.ogg");
 
     PlaySound(GameSound::cardPlace01);
     REQUIRE(IsSoundPlaying(testSound));
